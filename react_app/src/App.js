0f5fe8a62659c920d406d6d94519da61a1a62402
Paweł Miążek <pawel@linacti.pl>
2020-10-21 14:12:31 +0200

    Formatted code view

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,60 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Form from './components/Form';
-import TodoList from './components/TodoList';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
-
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState("all");
   const [filteredTodos, setFilteredTodos] = useState([]);
 
   const filterHandler = () => {
-    switch(status){
-      case 'completed':
-        setFilteredTodos(todos.filter((todo) => todo.completed === true))
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
         break;
-      case 'uncompleted':
-        setFilteredTodos(todos.filter((todo) => todo.completed === false))
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
         break;
       default:
         setFilteredTodos(todos);
         break;
     }
-  }
+  };
 
   useEffect(() => {
     getLocalTodos();
-  }, [])
+  }, []);
 
   useEffect(() => {
     filterHandler();
     saveLocalTodos();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [todos, status]);
 
   const saveLocalTodos = () => {
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
 
   const getLocalTodos = () => {
     if (localStorage.getItem("todos") === null) {
-      localStorage.setItem("todos", JSON.stringify([]))
+      localStorage.setItem("todos", JSON.stringify([]));
     } else {
-      let todoLocal = JSON.parse(localStorage.getItem("todos"))
-      setTodos(todoLocal)
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoLocal);
     }
-  }
+  };
 
   return (
     <div className="App">
       <header>
         <h1>My ToDo List</h1>
       </header>
-      <Form setStatus={setStatus} todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
-      <TodoList todos={todos} setTodos={setTodos} filteredTodos={filteredTodos} />
+      <Form
+        setStatus={setStatus}
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        filteredTodos={filteredTodos}
+      />
     </div>
-
   );
 }
 
--- react_app/src/components/TodoList.js
+++ react_app/src/components/TodoList.js
@@ -1,25 +1,22 @@
-import React from 'react'
-import Todo from './Todo'
+import React from "react";
+import Todo from "./Todo";
 
 function TodoList({ todos, setTodos, filteredTodos }) {
-
-
-
   return (
-      <div className="todo-container">
-        <ul className="todo-list">
-          {filteredTodos.map(todo => (
-            <Todo 
-              key={todo.id}
-              text={todo.text}
-              todo={todo}
-              setTodos={setTodos}
-              todos={todos}
-            />
-          ))}
-        </ul>
-      </div>
-  )
+    <div className="todo-container">
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo
+            key={todo.id}
+            text={todo.text}
+            todo={todo}
+            setTodos={setTodos}
+            todos={todos}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default TodoList
+export default TodoList;
